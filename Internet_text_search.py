2a67f1ff9c3ac196b8fb3372dcc7be439975ffd0
xiaolabop <40316750+xiaolabop@users.noreply.github.com>
2018-06-16 19:26:26 +0800

    Update Internet_text_search.py

--- Internet_text_search.py
+++ Internet_text_search.py
@@ -1,1 +1,4 @@
 
+# this module provide methods to search on various search engines and return parsed results
+# later we may add proxy pool to avoid search engine ban
+# we may also use boost method to jointly use search results from different search engines
